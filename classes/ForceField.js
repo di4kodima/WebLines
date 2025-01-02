2f23839cbcb464befc665992a43684da13fd17f6
di4ko <di4ko.dima@gmail.com>
2025-01-02 13:29:20 +0300

    Добавлен функционал для моделирования линий равных потенциалов

--- classes/ForceField.js
+++ classes/ForceField.js
@@ -15,13 +15,11 @@ export class ForceField
     ForceLinesPaint(count, step)
     {
         let PosCharges = this.ChargedObjects.filter(e => e.Charge > 0);
-        console.log(PosCharges);
         PosCharges.forEach(PosCharge => {
             
             for (let a = 0; a < Math.PI * 2; a += Math.PI * 2 / count)
                 {
                     let point = new Vector2(PosCharge.Position.X + Math.cos(a) * 5, PosCharge.Position.Y + Math.sin(a) * 5);
-                    console.log(point);
                     this.ForceLinePaint(point, step);
                 }
         });
@@ -29,7 +27,6 @@ export class ForceField
 
     ForceLinePaint(start,h, IterCount = 1000)
     {
-        console.log(start);
         let Vres = new Vector2(0,0);
         IterCount--;
         if (IterCount <= 0)
@@ -54,57 +51,70 @@ export class ForceField
         let a = h;
 
         Vres.Normalize();
-        // if (start.X > this.Canvas.weght + h || start.X + h < 0 || start.Y > this.Canvas.height + h || start.Y < 0)
-        //     a = 100;
+        if (start.X > this.Canvas.weght + h || start.X + h < 0 || start.Y > this.Canvas.height + h || start.Y < 0)
+            a = 100;
 
         Vres.X *= a;
         Vres.Y *= a;
 
         if (!(start.X > this.Canvas.weght || start.X < 0 || start.Y > this.Canvas.height || start.Y < 0))
            {
-            console.log("DRAW"); 
             CanvasDrawer.DrawLine(this.Canvas ,new Vector2(start.X, start.Y), new Vector2(start.X + Vres.X, start.Y + Vres.Y), 'red');
            }
 
         this.ForceLinePaint(new Vector2(start.X + Vres.X, start.Y + Vres.Y), h, IterCount);
     }
-
-    // Rastr( x,  y,  h,  val)
-    // {
-    //     List<Point> points = new List<Point>();
-
-    //     point = new Point();
-
-    //     if (OnLine(x, x + h, y, y, h, val, out point))
-    //     {
-    //         points.Add(point);
-    //     }
-
-    //     if (OnLine(x + h, x + h, y, y + h, h, val, out point))
-    //     {
-    //         points.Add(point);
-    //     }
-
-    //     if (OnLine(x, x, y, y + h, h, val, out point))
-    //     {
-    //         points.Add(point);
-    //     }
-    //     if (OnLine(x, x + h, y + h, y + h, h, val, out point))
-    //     {
-    //         points.Add(point);
-    //     }
-    //     if (points.Count == 2)
-    //     {
-    //         printLine(new Vector(points[0].X, points[0].Y), new Vector(points[1].X, points[1].Y), Potentialbrush);
-    //     }
-    // }
-
-    OnLine( x1,  x2,  y1,  y2,  h,  v,   p)
+    
+    EquipotentialLinesPaint(MinField, MaxField, LinesCount, h)
     {
-        f1 = func(x1, y1);
-        f2 = func(x2, y2);
-        min = Math.Min(f1, f2);
-        max = Math.Max(f1, f2);
+        if (this.ChargedObjects.Count == 0) return;
+
+        let values = [];
+
+        for (let i = 1; i < LinesCount + 1; i++)
+        {
+            values.push(MinField + ((MaxField - MinField) / LinesCount * i));
+        }
+        
+        for (let i = 0; i < 1200 / h; i++)
+        {
+            for (let j = 0; j < 800 / h; j++)
+            {
+                values.forEach(val =>
+                {
+                    this.Rastr(0 + h * i, 0 + h * j, h, val);
+                    this.Rastr(0 + h * i, 0 + h * j, h, -val);
+                });
+            }
+        }
+    }
+
+    Rastr( x,  y,  h,  val)
+    {
+        let points = [];
+
+        let point = new Vector2(0,0);
+
+        points.push(this.OnLine(x, x + h, y, y, h, val));
+        points.push(this.OnLine(x + h, x + h, y, y + h, h, val));
+        points.push(this.OnLine(x, x + h, y + h, y + h, h, val));
+        points.push(this.OnLine(x, x, y, y + h, h, val));
+
+        points = points.filter(p => p);
+        if (points.length == 2)
+        {
+            CanvasDrawer.DrawLine(this.Canvas,new Vector2(points[0].X, points[0].Y), new Vector2(points[1].X, points[1].Y), "blue");
+        }
+    }
+
+    OnLine( x1,  x2,  y1,  y2,  h,  v)
+    {
+        let f1 = this.func(new Vector2(x1, y1));
+        let f2 = this.func(new Vector2(x2, y2));
+        let min = Math.min(f1, f2);
+        let max = Math.max(f1, f2);
+        let p = new Vector2(0,0);
+
         if (x1 != x2)
             if (f1 < f2)
                 p.X = x1 + h * (v - min) / (max - min);
@@ -120,7 +130,18 @@ export class ForceField
                 p.Y = y1 + h * ((v - min) / (max - min));
         else
             p.Y = y1;
-        return (v <= max && v > min);
+        if(v <= max && v > min)
+            return p;
+        else return null;
+    }
+
+    func(Position)
+    {
+        let Force = 0;
+        this.ChargedObjects.forEach(charge =>
+            Force += charge.GetForce(Position)
+        );
+        return Force;
     }
 }
 
--- index.js
+++ index.js
@@ -12,6 +12,11 @@ let BrushHandler = new BrushesHandler(Canvas, Field);
 var ForceLinesCountInput = document.getElementById("ForceLinesCountInput");
 var ForceLinesStepInput = document.getElementById("ForceLinesStepInput");
 
+var EqviLinesStepInput = document.getElementById("EqviLinesStepInput");
+var EqviLinesCountInput = document.getElementById("EqviLinesCountInput");
+var EqviLinesMinForceInput = document.getElementById("EqviLinesMinForceInput");
+var EqviLinesMaxForceInput = document.getElementById("EqviLinesMaxForceInput");
+
 var ClearButton = document.getElementById("ClearButton");
 ClearButton.addEventListener('click', function(event)
 {
@@ -29,6 +34,7 @@ PaintButton.addEventListener('click', function(event)
         element.Paint(Canvas);
     });
     Field.ForceLinesPaint(ForceLinesCountInput.value, ForceLinesStepInput.value);
+    Field.EquipotentialLinesPaint(+EqviLinesMinForceInput.value,+EqviLinesMaxForceInput.value,+EqviLinesCountInput.value,+EqviLinesStepInput.value);
 })
 
 var ChargeValueInput = document.getElementById('ChargeValueInput');
